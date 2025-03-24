ddad4eeefa4548abf4fdbacd78ff089523db7189
BeginerLord <133072912+BeginerLord@users.noreply.github.com>
2025-03-24 18:43:49 -0500

    added docs swagger

--- src/main/java/com/bookLibrary/rafapcjs/author/presentation/controllers/AuthorContoller.java
+++ src/main/java/com/bookLibrary/rafapcjs/author/presentation/controllers/AuthorContoller.java
@@ -4,6 +4,7 @@ import com.bookLibrary.rafapcjs.author.presentation.dtos.AuthorDto;
 import com.bookLibrary.rafapcjs.author.presentation.payload.AuthorPayload;
 import com.bookLibrary.rafapcjs.author.service.interfaces.IAuthorService;
 import com.bookLibrary.rafapcjs.commons.utils.pageable.PageableUtil;
+import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -23,12 +24,14 @@ public class AuthorContoller {
     private final IAuthorService authorService;
 
     @PostMapping
+    @Operation(summary = "Create a new author", description = "Creates a new author with the provided details")
     public ResponseEntity<?> create(@Valid @RequestBody AuthorPayload authorPayload) throws URISyntaxException {
         authorService.save(authorPayload);
         return ResponseEntity.created(new URI("/api/v1/author/")).build();
     }
 
     @GetMapping
+    @Operation(summary = "Get all authors", description = "Retrieves a paginated list of all authors")
     public ResponseEntity<?> getAll(
             @RequestParam(defaultValue = "0") int page,
             @RequestParam(defaultValue = "10") int size,
@@ -40,24 +43,28 @@ public class AuthorContoller {
     }
 
     @PutMapping("/update/{uuid}")
+    @Operation(summary = "Update an author", description = "Updates the details of an existing author")
     public ResponseEntity<?> update(@Valid @RequestBody AuthorPayload authorPayload, @PathVariable UUID uuid) {
         authorService.update(authorPayload, uuid);
         return ResponseEntity.noContent().build();
     }
 
     @DeleteMapping("/{uuid}")
+    @Operation(summary = "Delete an author", description = "Deletes an author by UUID")
     public ResponseEntity<?> delete(@PathVariable UUID uuid) {
         authorService.deleteByUuid(uuid);
         return ResponseEntity.noContent().build();
     }
 
     @GetMapping("/fullName/{fullName}")
+    @Operation(summary = "Find author by full name", description = "Retrieves an author by their full name")
     public ResponseEntity<?> findByFullName(@PathVariable String fullName) {
         AuthorDto authorDto = authorService.findByFullName(fullName);
         return ResponseEntity.ok(authorDto);
     }
 
     @GetMapping("/uuid/{uuid}")
+    @Operation(summary = "Find author by UUID", description = "Retrieves an author by their UUID")
     public ResponseEntity<?> findByUuid(@PathVariable UUID uuid) {
         AuthorDto authorDto = authorService.findByUuid(uuid);
         return ResponseEntity.ok(authorDto);
